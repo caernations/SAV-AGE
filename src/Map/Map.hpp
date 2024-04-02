50bf670b54037aa354b22ed257eb9a0cc6aebe1c
Muneki <justinprabakti2204@gmail.com>
2024-04-02 20:27:57 +0700

    edit : moved Map.cpp to Map.hpp for reasons

--- src/Map/Map.hpp
+++ src/Map/Map.hpp
@@ -2,6 +2,8 @@
 #define MAP_HPP
 
 #include <vector>
+#include <iomanip>
+#include <iostream>
 #include <string>
 #include "../Item/Item.hpp"
 
@@ -17,28 +19,76 @@ class Map{
         string name;
     public:
         // inisialisasi map kosong
-        Map();
+        Map(){
+
+        };
 
         // inisialisasi map dengan ukuran tertentu
-        Map(int mapX, int mapY);
+        Map(int mapX, int mapY){
+            this->lebar = lebar;
+            this->panjang = panjang;
+            map = vector<vector<T*>>(lebar, vector<T*>(panjang, nullptr));
+        };
 
         // the above + name
-        Map(int mapX, int mapY, const string& name);
+        Map(int mapX, int mapY, const string& name) : Map(mapX,mapY){
+            this->name = name;    
+        };
 
         // print map seperti spek
-        void displayMap();
+        void displayMap(){
+            iterateAlphabet(panjang);
+            print_divider(panjang,5);
+            for (int i = 0; i < lebar; i++){
+            cout << setw(2) << setfill('0') << i+1 << " ";
+            cout << "|";
+            for (int j = 0; j < panjang; j++){
+                if (map[j][i] == nullptr){
+                    cout << "     " << "|";
+                } else {
+                    cout << " " << map[j][i]->getItemCode() << " " << "|";
+                }
+            }
+            cout << endl;
+            print_divider(panjang,5);
 
-        vector<vector<T*>> getMap();
+            }
+        };
+
+        vector<vector<T*>> getMap(){
+           return map;
+        };
 
         // meng set suatu item ke map, menerima OBJEK, bukan konstanta
-        void set(int x, int y, T* item);
+        void set(int x, int y, T* item){
+            this->map[x][y] = item;
+        };
 
         //debug tools : cek ukuran map dari ukuran list
-        void reportsize();
+        void reportsize(){
+            cout << "MAP SIZE " << map.size() << "*" << map[0].size();
+        };
         
-        void print_divider(int items, int size);
+        void print_divider(int items, int size){
+        cout << "   +";
+        for (int i = 0; i < items; i++){
+            for (int j = 0; j < size; j++){
+                cout << "-";
+            }
+            cout << "+";
+        }
+        cout << endl;
+        };
 
-        void iterateAlphabet(int length);
+        void iterateAlphabet(int length){
+        char startChar = 'A';
+        cout << "    ";
+        for (int i = 0; i < length; ++i) {
+            cout << "  " <<  static_cast<char>(startChar + i) << "  ";
+            cout << " ";
+        }
+        cout << std::endl;
+        };
 
 
 };
